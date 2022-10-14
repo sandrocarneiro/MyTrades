500c283e3f988e77764a33e8f4301ee4cee273a7
Sandro Brandão Carneiro <sandro_brandao@hotmail.com>
2022-10-14 08:56:26 -0300

    Correção de bugs

--- WebApp/Models/ResumoDiarioViewModel.cs
+++ WebApp/Models/ResumoDiarioViewModel.cs
@@ -4,16 +4,16 @@ namespace WebApp.Models
 {
     public class ResumoDiarioViewModel
     {
-        public string DataOperacao { get; set; }
-        public string Total { get; set; }
+        public DateTime DataOperacao { get; set; }
+        public decimal Total { get; set; }
         public ResumoDiarioViewModel()
         {
 
         }
         public ResumoDiarioViewModel(DateTime dataOperacao, decimal valor)
         {
-            this.DataOperacao = dataOperacao.ToString("dd/MM/yyyy");
-            this.Total = String.Format("{0: #, ##0.00; (#,##0.00)} ", valor);
+            this.DataOperacao = dataOperacao;
+            this.Total = valor;
         }
     }
 }
